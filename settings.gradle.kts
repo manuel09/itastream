5cbf62ed1ba496f86590e85409f6e1bc50886824
manuel09 <manu.17@live.it>
2025-04-09 18:29:55 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "CloudstreamPlugins"
+
+// This file sets what projects are included. Every time you add a new project, you must add it
+// to the includes below.
+
+// Plugins are included like this
+include(
+    "AnimeUnity",
+    "AnimeWorld",
+    "CalcioStreaming",
+    "CB01",
+    "DaddyLive",
+    "Huhu",
+    "OnlineSerieTV",
+    "StreamingCommunity",
+    "TV",
+    "ToonItalia",
+    "YouTube"
+)
